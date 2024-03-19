65090a2ba74074fb5a1c6d475c02e0a08825d49e
aleksandar <alpe88.dev@gmail.com>
2024-03-18 18:44:23 -0700

    added constant starter

---
+++ server/constants.js
@@ -0,0 +1,21 @@
+const HOST = "";
+const PORT = "";
+const AUTH_USER = "";
+const AUTH_PASSWORD = "";
+
+const FROM = "";
+const TO = "";
+const SUBJECT = "";
+
+const EMAIL_TEXT = "";
+
+module.exports = {
+  HOST,
+  PORT,
+  AUTH_USER,
+  AUTH_PASSWORD,
+  FROM,
+  TO,
+  SUBJECT,
+  EMAIL_TEXT,
+};
