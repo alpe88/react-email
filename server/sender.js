31c2ed59e3ceb131550dfba95867c817d77dad2c
aleksandar <alpe88.dev@gmail.com>
2024-03-18 18:44:09 -0700

    added nodemailer sender

---
+++ server/sender.js
@@ -0,0 +1,36 @@
+import { render } from "@react-email/render";
+import nodemailer from "nodemailer";
+import { Email } from "./email";
+import {
+  HOST,
+  PORT,
+  AUTH_USER,
+  AUTH_PASSWORD,
+  FROM,
+  TO,
+  SUBJECT,
+  EMAIL_TEXT,
+} from "./constants";
+
+const transporter = nodemailer.createTransport({
+  host: HOST,
+  port: PORT,
+  secure: true,
+  auth: {
+    user: AUTH_USER,
+    pass: AUTH_PASSWORD,
+  },
+});
+
+const msg = EMAIL_TEXT;
+
+const emailHtml = render(Email({ msg }));
+
+const options = {
+  from: FROM,
+  to: TO,
+  subject: SUBJECT,
+  html: emailHtml,
+};
+
+await transporter.sendMail(options);
